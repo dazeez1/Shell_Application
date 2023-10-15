d808de77c4371eb1acd80ab000b6fc55dac66baa
Belinda1704 <b.larose@alustudent.com>
2023-10-15 07:37:26 -0700

    changes

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -6,4 +6,4 @@ mkdir -p "negpod-18_q1"
 # Move the files to the target directory
 mv main.sh "students-list_0923.txt" select-emails.sh "student-emails.txt" "negpod_18-q1"/
 
-echo "Files moved to negpod_18-q1.
+echo "Files moved to negpod_18-q1."
